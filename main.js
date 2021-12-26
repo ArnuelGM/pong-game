87791929d8f6d1db80e6d4721abf703542a86f5c
Arnuel Gutierrez Menco <argume31@hotmail.com>
2021-12-26 06:08:55 +0000

    js1k challenge 3er attempt (1.1k [988B])

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 ((d) => {
   Ab               = Math.abs
-  AEL              = 'addEventListener'
-  FS               = 'fillStyle'
-  BP               = 'beginPath'
-  CP               = 'closePath'
+  L              = 'addEventListener'
+  S               = 'fillStyle'
+  B               = 'beginPath'
+  C               = 'closePath'
   R                = 'rect'
   F                = 'fill'
-  KC               = 'keyCode'
+  K               = 'keyCode'
   let ctx          = li.getContext('2d') // li = canvas (view id in html)
   let canvasW      = 1e3
   let canvasH      = 564.97;
-  // Borra el lienzo
-  (bc = () => {
-    li.width   = canvasW
-    li.height  = canvasH
-  })()
   let ballColor    = '#1976d2'
   let playersColor = 'blue'
   let boardColor   = '#222'
@@ -33,62 +28,41 @@
   let leftMoving   = 0
   let rightMoving  = 0
   let turn         = 'l'
-  d[AEL]('keydown', (t) => {
-    k = t[KC]
+  d[L]('keydown', (t) => {
+    k = t[K]
+    z = k==38 || k==87 ? -1 : 1
     // Poner en movimiento la bola
     // Tecla espaciadora
-    if(k==32){ballIsMoving = 1; return;}
-    // Player left top
-    // Tecla w
-    if(k==87){leftMoving = 1; leftSpeed = -Ab(leftSpeed); return;}
-    // Player left bottom
-    // Tecla s
-    if(k==83){leftMoving = 1; leftSpeed = Ab(leftSpeed); return;}
-    // Player right top
-    // Flecha arriba
-    if(k==38){rightMoving = 1; rightSpeed  = -Ab(rightSpeed); return;}
-    // Player right bottom
-    // Flecha abajo
-    if(k==40){rightMoving = 1; rightSpeed  = Ab(rightSpeed); return;}
+    if(k==32) ballIsMoving = 1
+    // Player left move
+    if(k==87 || k==83){leftMoving = 1; leftSpeed = (Ab(leftSpeed) * z);}
+    // player roght move
+    if(k==38 || k==40){rightMoving = 1; rightSpeed  = (Ab(rightSpeed) * z);}
   })
-  d[AEL]('keyup', (t) => {
-    k = t[KC]
-    // Player left top
-    if(k==87){leftMoving = 0; return;}
-    // Player left bottom
-    if(k==83){leftMoving = 0; return;}
-    // Player right top
-    if(k==38){rightMoving = 0; return;}
-    // Player right bottom
-    if(k==40){rightMoving = 0; return;}
+  d[L]('keyup', (t) => {
+    k = t[K]
+    // Player left stop moving
+    if(k==87 || k==83) leftMoving = 0
+    // Player right stop moving
+    if(k==38 || k==40) rightMoving = 0
   })
-  // Pinta el tablreo con los parametros por defecto
-  pt = () => {
-    ctx[FS] = boardColor
+  // Bucle principal del Juego
+  setInterval(() => {
+    // Borra el lienzo
+    li.width   = canvasW
+    li.height  = canvasH
+
+    // Pinta el tablreo con los parametros por defecto
+    ctx[S] = boardColor
     ctx[R](0,0,canvasW,canvasH)
     ctx[F]()
-  }
-  // Pintar la posicion de los jugadores
-  pj = () => {
-    ctx[FS] = playersColor
-    // Player left
-    ctx[BP]()
-    ctx[R](10, leftTop, 3, playerLength)
-    ctx[F]()
-    ctx[CP]()
-    // Player right
-    ctx[BP]()
-    ctx[R](canvasW-14, rightTop, 3, playerLength)
-    ctx[F]()
-    ctx[CP]()
-  }
-  // Mover jugadores
-  mp = () => {
+
+    // Mover jugadores
     // Mover jugador de la izquierda
     if( leftMoving ) {
       leftTop += leftSpeed
       // Impedir que se salga por arriba
-      if( leftTop - Ab(leftSpeed) < 1 ) leftTop = 1
+      if( leftTop - Ab(leftSpeed) < 0 ) leftTop = 0
       // Impedir que se salga por abajo
       if( leftTop + Ab(leftSpeed) + playerLength > canvasH - 2 ) leftTop = canvasH - playerLength - 2
     }
@@ -96,55 +70,61 @@
     if( rightMoving ) {
       rightTop += rightSpeed
       // Impedir que se salga por arriba
-      if( rightTop - Ab(rightSpeed) < 1 ) rightTop = 1
+      if( rightTop - Ab(rightSpeed) < 0 ) rightTop = 0
       // Impedir que se salga por abajo
       if( rightTop + Ab(rightSpeed) + playerLength > canvasH - 2 ) rightTop = canvasH - playerLength - 2
     }
-  }
-  // Pintar la posicion de la bola
-  pb = () => {
-    ctx[FS] = ballColor
-    ctx[BP]()
-    ctx.arc(ballX, ballY, ballSize, 0, 2 * Math.PI)
+
+    // Pintar la posicion de los jugadores
+    ctx[S] = playersColor
+    // Player left
+    ctx[B]()
+    ctx[R](0, leftTop, 6, playerLength)
     ctx[F]()
-    ctx[CP]()
-  }
-  // Mover la bola por el tablero
-  bm = () => {
+    ctx[C]()
+    // Player right
+    ctx[B]()
+    ctx[R](canvasW-6, rightTop, 6, playerLength)
+    ctx[F]()
+    ctx[C]()
+    
+    // Mover la bola por el tablero
     if( ballIsMoving ) {
       ballX += ballSpeedX
       ballY += ballSpeedY
-      if( (ballX + (middleBall) >= canvasW - 1) || (ballX + (middleBall) >= (canvasW - 20) && ballY > rightTop && ballY < rightTop+playerLength ) ) ballSpeedX = -Ab(ballSpeedX)
-      else if((ballX - (middleBall) <= 0) || (ballX - (middleBall) <= 18 && ballY > leftTop && ballY < leftTop+playerLength )) ballSpeedX = Ab(ballSpeedX)
-      if( ballY + (middleBall) + 2 >= canvasH - 2 ) ballSpeedY = -Ab(ballSpeedY)
-      else if( ballY - (middleBall) < 5 ) ballSpeedY = Ab(ballSpeedY)
+      if( (ballX + (middleBall) >= canvasW) || (ballX + (middleBall) > (canvasW - 6) && ballY > rightTop && ballY < rightTop+playerLength ) ) ballSpeedX = -Ab(ballSpeedX)
+      else if((ballX - (middleBall) <= 0) || (ballX - (middleBall) < 6 && ballY > leftTop && ballY < leftTop+playerLength )) ballSpeedX = Ab(ballSpeedX)
+      if( ballY + (middleBall) >= canvasH ) ballSpeedY = -Ab(ballSpeedY)
+      else if( ballY - (middleBall) <= 0 ) ballSpeedY = Ab(ballSpeedY)
+    }
+    else if( turn == 'l' ) {
+      ballX = ballSize + 6
+      ballY = leftTop + (playerLength/2)
     }
     else {
-      if( turn == 'l' ) {
-        ballX = 13 + ballSize
-        ballY = leftTop + (playerLength/2)
-        return
-      }
-      ballX = canvasW - 10 - 4 - ballSize
+      ballX = canvasW - ballSize - 6
       ballY = rightTop + (playerLength/2)
     }
-  }
-  // Check left lost
-  cll =  () => {
-    if( ballX - middleBall < 10 ) {
+
+    // Pintar la posicion de la bola
+    ctx[S] = ballColor
+    ctx[B]()
+    ctx.arc(ballX, ballY, ballSize, 0, 2 * Math.PI)
+    ctx[F]()
+    ctx[C]()
+
+    // Check if left lost
+    if( ballX - middleBall < 4 ) {
       turn = 'l'
       ballIsMoving = 0
       ballSpeedX = Ab(ballSpeedX)
     }
-  }
-  // Check right lost
-  crl =  () => {
-    if( ballX + middleBall > canvasW-14 ) {
+
+    // Check if right lost
+    if( ballX + middleBall > canvasW-4 ) {
       turn = 'r'
       ballIsMoving = 0
       ballSpeedX = -Ab(ballSpeedX)
     }
-  }
-  // Bucle principal del Juego
-  setInterval(() => {bc();pt();mp();pj();bm();pb();cll();crl();}, 6.66)
+  }, 6.66)
 })(document)
